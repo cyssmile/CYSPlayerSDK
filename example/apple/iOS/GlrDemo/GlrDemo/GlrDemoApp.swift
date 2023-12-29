3d3de11cb23fa29596e3752662da6d825078d2d8
chys.charlie <chys.charlie@bytedance.com>
2023-12-29 17:53:15 +0800

    add iOS demo

---
+++ example/apple/iOS/GlrDemo/GlrDemo/GlrDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlrDemoApp.swift
+//  GlrDemo
+//
+//  Created by ByteDance on 2023/12/29.
+//
+
+import SwiftUI
+
+@main
+struct GlrDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
